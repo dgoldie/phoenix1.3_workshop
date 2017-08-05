ce181d4a866944eb53a24ee18ed46201fd09609f
Doug Goldie <dgoldie15@gmail.com>
2017-08-05 13:13:09 -0700

    initial commit

---
+++ dev_app/lib/dev_app/application.ex
@@ -0,0 +1,31 @@
+defmodule DevApp.Application do
+  use Application
+
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the Ecto repository
+      supervisor(DevApp.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(DevAppWeb.Endpoint, []),
+      # Start your own worker by calling: DevApp.Worker.start_link(arg1, arg2, arg3)
+      # worker(DevApp.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: DevApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    DevAppWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
